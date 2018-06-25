12d725a297999d369a439d3d06e993d72b0afa5f
Jorge Nunez Newton <jorgenewt@yahoo.com>
2018-06-24 21:39:44 -0400

    Prueba de index()
    Prueba sencilla

--- src/test/java/com/service/climatic/ControllerTest.java
+++ src/test/java/com/service/climatic/ControllerTest.java
@@ -5,22 +5,33 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ClimaHomeController.class)
-@ContextConfiguration
 @WebAppConfiguration
 public class ControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private ClimaHomeController controller;
+
+    @Test
+    public void index() throws Exception {
+        assertThat(controller).isNotNull();
+        assertThat(controller.index()).isNotNull();
+    }
 
     @Test
     public void getClima() throws Exception {
